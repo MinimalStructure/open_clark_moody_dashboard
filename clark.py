656f54060a629052f9eaf9d9bfc53d93528485ba
Minimal Structure <69333458+MinimalStructure@users.noreply.github.com>
2020-08-21 08:13:07 -0700

    Add files via upload

---
+++ clark.py
@@ -0,0 +1,10 @@
+#! python3
+#Clark Moody's Dashboard
+#This script opens up the Claark Moody Dashboard by hitting window r + clark
+
+import webbrowser
+
+#you could really use this to open any website, by changing the URL, but I like this one
+webbrowser.open('https://bitcoin.clarkmoody.com/dashboard/')
+
+#You can donate to Clark Moody here:  https://bitcoin.clarkmoody.com/donate/
